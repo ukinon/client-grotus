b661921c325721e875d466660bfc2c84880dc569
Arfiano Jordhy <arfianoj@gmail.com>
2024-06-21 13:03:06 +0700

    make tabs background in transactions page white

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -15,7 +15,7 @@ export default function TransactionPage() {
           defaultValue="unpaid"
           className="w-screen flex flex-col items-end"
         >
-          <TabsList>
+          <TabsList className="bg-white">
             <TabsTrigger value="unpaid">Belum bayar</TabsTrigger>
             <TabsTrigger value="paid">Sudah bayar</TabsTrigger>
           </TabsList>
