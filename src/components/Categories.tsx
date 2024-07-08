1e0a0fba7b2e9ba90afd08b53bc38194bacdc135
Arfiano Jordhy <arfianoj@gmail.com>
2024-07-08 08:49:22 +0700

    update home page for desktop

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -25,8 +25,8 @@ export default function Categories() {
 
   return (
     <div className="flex flex-col gap-4 pb-3 pt-6 w-[100%] ">
-      <h1 className="text-2xl font-bold pl-8">Jenis Nutrisi</h1>
-      <div className="flex flex-row md:flex-wrap gap-2 items-center justify-start  md:py-1 md:px-6 pr-8 overflow-x-auto w-screen pl-8">
+      <h1 className="text-2xl font-bold pl-8 md:pl-16">Jenis Nutrisi</h1>
+      <div className="flex flex-row md:flex-wrap gap-2 items-center justify-start  md:py-1 md:px-6 pr-8 overflow-x-auto w-screen pl-8 md:pl-16">
         {nutritionLoading &&
           Array(8)
             .fill(null)
